646e548cf507b8346f752c60b9b6f911c8624d11
0smanturgut <osmanturgutads@gmail.com>
2025-05-16 22:42:00 +0300

    Update fetch_tweets.py

--- utils/fetch_tweets.py
+++ utils/fetch_tweets.py
@@ -1,13 +1,21 @@
-import snscrape.modules.twitter as sntwitter
+import subprocess
+import json
 
 def get_latest_tweets(username, limit=5):
+    command = [
+        'snscrape',
+        '--jsonl',
+        f'--max-results={limit}',
+        f'twitter-user {username}'
+    ]
+    result = subprocess.run(command, capture_output=True, text=True)
+    tweets_raw = result.stdout.strip().split("\n")
     tweets = []
-    for i, tweet in enumerate(sntwitter.TwitterUserScraper(username).get_items()):
-        if i >= limit:
-            break
+    for line in tweets_raw:
+        tweet_data = json.loads(line)
         tweets.append({
-            'id': tweet.id,
-            'content': tweet.content,
-            'url': tweet.url
+            'id': tweet_data['id'],
+            'content': tweet_data['content'],
+            'url': tweet_data['url']
         })
     return tweets
